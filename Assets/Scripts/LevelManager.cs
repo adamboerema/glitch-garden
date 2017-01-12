44b28f9dfec1163619e5271d200ccb268d6db80d
Adam Boerema <Adam Boerema>
2017-01-12 01:05:19 -0500

    Initial setup for glitch garden

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNexLevelAfter;
+
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNexLevelAfter);
+    }
+
+	public void LoadLevel(string name) {
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequested() {
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
