aa32580962b5cc2b3efb4f75c1a4297fa5ef571c
Adam Boerema <adamboerema@gmail.com>
2017-01-25 23:32:04 -0500

    Added in animations and animation events for attacking and walking

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+    [Range (-1f, 1.5f)]
+    public float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rigidBody = gameObject.AddComponent<Rigidbody2D>();
+        rigidBody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter2D ()
+    {
+        Debug.Log(name + " trigger enter");
+    }
+
+    public void SetSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        Debug.Log("Hit target for " + damage);
+    }
+}
