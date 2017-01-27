d1d7f72e4e5451eb28900eb9d5762735a108b544
Adam Boerema <adamboerema@gmail.com>
2017-01-26 23:40:31 -0500

    Working attacking interactions and animation triggers

---
+++ Assets/Scripts/Attackers/Fox.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Attacker))]
+public class Fox : MonoBehaviour {
+
+    private Animator anim;
+    private Attacker attacker;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+        attacker = gameObject.GetComponent<Attacker>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        GameObject obj = collider.gameObject;
+        if (!obj.GetComponent<Defender>())
+        {
+            return;
+        }
+
+        if (obj.GetComponent<Stone>())
+        {
+            anim.SetTrigger("JumpTrigger");
+        } else
+        {
+            anim.SetBool("isAttacking", true);
+            attacker.Attack(obj);
+        }
+        Debug.Log("Fox collided with" + collider);
+
+    }
+}
