bd9c3dc312d59bbfe91fee8f976a4ff502eb7a67
Adam Boerema <adamboerema@gmail.com>
2017-01-30 23:32:22 -0500

    Added defender firing state generation and detection

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+    public GameObject projectile;
+    public GameObject projectileParent;
+    public GameObject gun;
+
+	private void Fire()
+    {
+        GameObject projectileInstance = Instantiate(projectile) as GameObject;
+        projectileInstance.transform.parent = projectileInstance.transform;
+        projectileInstance.transform.position = gun.transform.position;
+    }
+}
