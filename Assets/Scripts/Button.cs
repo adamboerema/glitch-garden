141a17a4c66e6ad87635a9a1072fd28745cf8d96
Adam Boerema <adamboerema@gmail.com>
2017-01-31 22:42:34 -0500

    Added projectile collions, cleaned up projectile parent

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public GameObject defenderPreab;
+    private static GameObject selectedDefender;
+    private Button[] buttonArray;
+
+	// Use this for initialization
+	void Start () {
+        buttonArray = GameObject.FindObjectsOfType<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnMouseDown()
+    {
+        foreach(Button btn in buttonArray)
+        {
+            btn.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+    
+        GetComponent<SpriteRenderer>().color = Color.white;
+        selectedDefender = defenderPreab;
+    }
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,13 +4,22 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 
     public GameObject projectile;
-    public GameObject projectileParent;
     public GameObject gun;
+    private static GameObject projectileParent;
+
+    void Start()
+    {
+        projectileParent = GameObject.Find("Projectiles") as GameObject;
+        if(!projectileParent)
+        {
+            projectileParent = new GameObject("Projectiles");
+        }
+    }
 
 	private void Fire()
     {
         GameObject projectileInstance = Instantiate(projectile) as GameObject;
-        projectileInstance.transform.parent = projectileInstance.transform;
+        projectileInstance.transform.parent = projectileParent.transform;
         projectileInstance.transform.position = gun.transform.position;
     }
 }
